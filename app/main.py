6394b1f1dcb1c991f34aa76c318a0d5b2ce4412b
pasinsiri <pasin_plp0206@hotmail.com>
2025-11-23 19:03:01 +0700

    re-factor importing modules

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
+import os
+import segno
 from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import segno
 from shortuuid import uuid
 from .database import database, qrs
 from . import crud
-import os
